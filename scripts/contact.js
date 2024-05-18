8c1b9f63d4aa4e30c75e0a57a80c8d58c94f1544
Smirnov Mark <frostikoil@gmail.com>
2024-05-18 13:08:31 +0300

    delete useless files

--- scripts/contact.js
+++
@@ -1,7 +0,0 @@
-// let button = document.querySelector('.button');
-
-// button.addEventListener('click', function () {
-//   let randomElement = getRandomElement(phrases);
-//   smoothly(phrase, 'textContent', randomElement.text);
-//   smoothly(image, 'src', randomElement.image);
-// });
